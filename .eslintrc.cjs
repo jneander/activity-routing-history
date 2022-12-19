15ee97c6ed46ace22fa949ca787569180fd349a7
Jeremy Neander <jeremy.neander@gmail.com>
2022-12-19 09:12:31 -0600

    chore: replace eslint config from dev-lint library

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,51 @@
-const {configureEslint} = require('@jneander/dev-lint')
-
-module.exports = configureEslint({
-  browser: true,
-})
-
-module.exports.overrides.push({
+module.exports = {
   env: {
+    es6: true,
+    mocha: true,
     node: true,
   },
 
-  files: ['./config/**/*.js', './scripts/**/*.js'],
-})
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'plugin:eslint-comments/recommended',
+    'plugin:promise/recommended',
+  ],
+
+  globals: {},
+
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+
+      files: ['./.eslintrc.cjs'],
+    },
+
+    {
+      files: ['./**/*.spec.js', './**/_specs_/**/*.js'],
+
+      globals: {
+        expect: 'writable',
+      },
+    },
+  ],
+
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+
+  plugins: ['import', 'promise', 'mocha'],
+  root: true,
+
+  rules: {
+    'arrow-body-style': 'off',
+    'eslint-comments/no-unused-disable': 'error',
+    'import/extensions': ['error', 'ignorePackages', {js: 'never'}],
+    'import/no-extraneous-dependencies': ['error', {devDependencies: true}],
+    'no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
+    'prefer-arrow-callback': 'off',
+  },
+}
